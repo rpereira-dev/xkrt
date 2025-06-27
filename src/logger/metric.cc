202ff381f3feea348d4441b880d94b8a02c1bf9a
Romain PEREIRA <rpereira@anl.gov>
2025-06-27 14:54:28 +0000

    Fixed GB metric conversion + coherent 1D works

--- src/logger/metric.cc
+++ src/logger/metric.cc
@@ -54,9 +54,9 @@ xkrt_metric_byte(char * buffer, int bufsize, size_t nbytes)
     const char * suffixes[] = {"B", "KB", "MB", "GB", "TB", "PB", "EB"};
     unsigned int i = 0;
     double size = (double) nbytes;
-    while (size >= 1000 && i < sizeof(suffixes) / sizeof(*suffixes))
+    while (size >= 1024 && i < sizeof(suffixes) / sizeof(*suffixes))
     {
-        size /= 1000;
+        size /= 1024;
         i++;
     }
     snprintf(buffer, bufsize, "%.2lf%s", size, suffixes[i]);
@@ -84,9 +84,9 @@ xkrt_metric_bandwidth(char * buffer, int bufsize, size_t byte_per_sec)
     const char * suffixes[] = {"B", "KB", "MB", "GB", "TB", "PB", "EB"};
     unsigned int i = 0;
     double size = (double) byte_per_sec;
-    while (size >= 1000 && i < sizeof(suffixes) / sizeof(*suffixes))
+    while (size >= 1024 && i < sizeof(suffixes) / sizeof(*suffixes))
     {
-        size /= 1000;
+        size /= 1024;
         i++;
     }
     snprintf(buffer, bufsize, "%.2lf%s/s", size, suffixes[i]);
